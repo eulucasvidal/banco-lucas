75a47086154fc42d57e04bc4e75251159c7c120f
Lucas Vidal <luccas.lima12@gmail.com>
2023-11-06 13:38:40 -0300

    Atualizando os codigos do prototipo

--- js/modules/tooltip-menu.js
+++
@@ -1,8 +0,0 @@
-export default function initTooltipMenu(){
-    const dropMenu = document.querySelector('[data-menu="dropdown]');
-
-    function dropdownMenu(){
-        dropMenu.classList.add('dropativo');
-    }
-    dropMenu.addEventListener('mouseenter', dropdownMenu)
-}
--- js/script.js
+++ js/script.js
@@ -1,4 +1,2 @@
 import initToggleMenu from "./modules/menu-mobile.js";
-import initTooltipMenu from "./modules/tooltip-menu.js";
 initToggleMenu();
-initTooltipMenu();
