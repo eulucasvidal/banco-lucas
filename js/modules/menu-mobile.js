a686bbbb119f32ddb1add9d000e93c971a83cc3f
Lucas Vidal <luccas.lima12@gmail.com>
2023-11-13 20:41:06 -0300

    Adicionando scroll suave ao ativar o menu mobile

--- js/modules/menu-mobile.js
+++ js/modules/menu-mobile.js
@@ -3,9 +3,11 @@ export default function initToggleMenu() {
   const botaoMobile = document.querySelector('[data-menu="button"]');
 
   function openMenu(event) {
+    if (event.type === 'touchstart')event.preventDefault();
     menuMobile.classList.toggle("ativo");
     botaoMobile.classList.toggle("ativo");
   }
 
   botaoMobile.addEventListener("click", openMenu);
+  botaoMobile.addEventListener("touchstart", openMenu);
 }
